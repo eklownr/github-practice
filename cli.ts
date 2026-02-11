67a4c57b69c7bf3b946ce08e8b62f38a2555f899
eklownr <eklownr@gmail.com>
2026-02-11 17:27:20 +0100

    main loop

--- cli.ts
+++ cli.ts
@@ -1,16 +1,38 @@
 import inquirer from "inquirer";
 
-const mainMenu = async () => {
-	const answers = await inquirer.prompt([
-		{
-			type: "select",
-			name: "acction",
-			message: "What would you like to do?",
-			choices: ["View Trips", "Add Activity", "View Budget", "Exit"],
-		},
-	]);
-	if (answers) {
-		console.log(answers);
+// Main menu show trips, activity, budget, options.
+const mainMenu = async (): Promise<void> => {
+	let choice: string = "";
+
+	// Loop menu
+	while (choice !== "Exit") {
+		try {
+			const answers = await inquirer.prompt<{ action: string }>([
+				{
+					type: "select",
+					name: "action",
+					message: "What would you like to do?",
+					choices: [
+						"View Trips",
+						"Add Activity",
+						"View Budget",
+						"Exit",
+					],
+				},
+			]);
+			// Set choice to stop loop
+			choice = answers.action;
+
+			// Handle users options
+			console.log(answers.action);
+		} catch (error) {
+			// Hantera okända fel på ett typsäkert sätt
+			if (error instanceof Error) {
+				console.error("Menu error:", error.message);
+			} else {
+				console.error("Unknown error:", String(error));
+			}
+		}
 	}
 };
 
